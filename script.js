207104af07d8622d285a3744b2d3e12df8706f85
Craig Kaley <craig@caucusroom.com>
2020-04-29 19:28:57 -0600

    Work in progress save

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var entity_id = "305";
+var entity_type = "city";
+var apiKey = "75b4586940c208d07b537c70b9182501";
 
+var queryURL =
+  "https://developers.zomato.com/api/v2.1/search?apikey=" +
+  apiKey +
+  "&entity_type=" +
+  entity_type +
+  "&entity_id=" +
+  entity_id;
+
+console.log(queryURL);
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
