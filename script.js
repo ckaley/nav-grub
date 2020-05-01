4d5c5425ef3bfc7da2c08b7bb60d863de32815e5
Rick Levy <ricklevy21@gmail.com>
2020-05-01 10:51:21 -0600

    variables for data from api added

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ $(document).ready(function(){
 
 /*Restaurant Info Card: add this into the for-loop that within the ajax call that grabs data from the 10 restaurants in the API's response
 
-I added values (src & text) based on the response object, but we may want to create variables to store those
+I made variables for a few of the data records that we can get back from zomato API. we can change or add more or less, just getting a few examples in here.
+We are probably going to need some if statements, in case a data record is null (for example, if there is nothing listed in "cuisines").
 ----------------------*/
 //variables for variables from response
-
+var restaurantImage = response.restaurants[i].restaurant.featured_image;
+var restaurantName = response.restaurants[i].restaurant.name;
+var cuisines = response.restaurants[i].restaurant.cuisines;
+var hours = response.restaurants[i].restaurant.timings;
+var address = response.restaurants[i].restaurant.location.address;
+var neighborhood = response.restaurants[i].restaurant.location.locality;
 
 
 //element variables
@@ -23,14 +29,14 @@ var infoCardRow = $("<div>").attr("class", "row");
 var infoCardColL = $("<div>").attr("class", "col s4");
 var infoCardColR = $("<div>").attr("class", "col s8");
 var infoCardImage = $("<div>").attr("class", "card-image")
-var featuredImage = $("<img>").attr("src", response.restaurants[i].restaurant.featured_image);
+var featuredImage = $("<img>").attr("src", restaurantImage);
 var infoCardContent = $("<div>").attr("class", "card-content");
-var infoCardTitle = $("<span>").attr("class", "card-title").text(response.restaurants[i].restaurant.name);
+var infoCardTitle = $("<span>").attr("class", "card-title").text(restaurantName);
 var infoCardDetails = $("div");
-var infoCardCuisines = $("<p>").text("Cuisine Offerings: "+response.restaurants[i].restaurant.cuisines);
-var infoCardHours = $("<p>").text("Hours: "+response.restaurants[i].restaurant.timings);
-var infoCardAddress = $("<p>").text("Address: "+response.restaurants[i].restaurant.location.address);
-var infoCardHood = $("<p>").text("Neighborhood: "+response.restaurants[i].restaurant.location.locality);
+var infoCardCuisines = $("<p>").text("Cuisine Offerings: "+cuisines);
+var infoCardHours = $("<p>").text("Hours: "+hours);
+var infoCardAddress = $("<p>").text("Address: "+address);
+var infoCardHood = $("<p>").text("Neighborhood: "+neighborhood);
 var infoCardMenu = $("<a>").attr("class", "waves-effect waves-light btn", "id", "menuBtn").text("VIEW FULL MENU");
 var infoCardMenuIcon = $("<i>").attr("class", "material-icons right").text("restaurant_menu");
 var infoCardCall = $("<a>").attr("class", "waves-effect waves-light btn", "id", "callBtn").text("CALL");
